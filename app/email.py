f221ce1f8f8706563c76aa43bb53fb3f0bd9c26e
Martin Kondor <martinkondor@gmail.com>
2024-10-11 14:45:30 +0200

    emails

--- app/email.py
+++ app/email.py
@@ -1,29 +1,47 @@
 from flask import render_template
 from flask_mail import Message
 from app import mail
-from threading import Thread
 from flask import current_app
 
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
-
-def send_email(subject, sender, recipients, text_body, html_body):
-    msg = Message(subject, sender=sender, recipients=recipients)
-    msg.body = text_body
-    msg.html = html_body
-    Thread(target=send_async_email, args=(current_app._get_current_object(), msg)).start()
 
 def send_welcome_email(user):
-    send_email('Welcome to BoxNinja',
-               sender=current_app.config['MAIL_DEFAULT_SENDER'],
-               recipients=[user.email],
-               text_body="",
-               html_body=render_template('email/welcome.html', user=user))
+    subject = "Üdvözöljük a BoxNinja-nál!"
+    recipients = [user.email]
+    html_body = render_template(
+        "email/welcome.html", user=user, base_url=current_app.config["BASE_URL"]
+    )
+    send_email(subject, recipients, html_body)
 
-def send_login_notification(user):
-    send_email('Login Notification',
-               sender=current_app.config['MAIL_DEFAULT_SENDER'],
-               recipients=[user.email],
-               text_body="",
-               html_body=render_template('email/login_notification.html', user=user))
+
+def send_login_notification(user, login_date, login_ip, login_device):
+    subject = "Új bejelentkezés észlelve"
+    recipients = [user.email]
+    html_body = render_template(
+        "email/login_notification.html",
+        user=user,
+        login_date=login_date,
+        login_ip=login_ip,
+        login_device=login_device,
+        base_url=current_app.config["BASE_URL"],
+    )
+    send_email(subject, recipients, html_body)
+
+
+def send_delivery_notification(user, tracking_number):
+    subject = "Csomagja megérkezett a BoxNinja-hoz"
+    recipients = [user.email]
+    html_body = render_template(
+        "email/delivery_notification.html",
+        user=user,
+        tracking_number=tracking_number,
+        base_url=current_app.config["BASE_URL"],
+    )
+    send_email(subject, recipients, html_body)
+
+
+def send_email(subject, recipients, html_body):
+    msg = Message(
+        subject, sender=current_app.config["MAIL_DEFAULT_SENDER"], recipients=recipients
+    )
+    msg.html = html_body
+    mail.send(msg)
