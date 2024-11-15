1b421e141e3f844e34cf12d100a5e4ad0f4d8423
Martin Kondor <martinkondor@gmail.com>
2024-11-15 15:13:55 +0100

    db admin route

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -4,6 +4,7 @@ from app.models import User
 from app.decorators import admin_required
 import json
 from app import db
+from app.models import Package, Courier, Warehouse, Assignment, ParcelLocker
 
 
 bp = Blueprint("admin", __name__)
@@ -24,19 +25,40 @@ def users():
     users = User.query.all()
     return render_template("admin/users.html", title="Felhasználó kezelés", users=users)
 
-@bp.route("/admin/change_permission", methods=['POST'])
+
+@bp.route("/admin/change_permission", methods=["POST"])
 @login_required
 @admin_required
 def change_permission():
-    data=json.loads(request.data)
-    userId=data['userId']
-    new_permission=data['new_permission']
-    user=User.query.get(userId)
+    data = json.loads(request.data)
+    userId = data["userId"]
+    new_permission = data["new_permission"]
+    user = User.query.get(userId)
     print(userId, new_permission)
 
-    user.role=new_permission
+    user.role = new_permission
     db.session.add(user)
     db.session.commit()
 
 
+@bp.route("/db")
+@login_required
+@admin_required
+def db_view():
+    users = User.query.all()
+    packages = Package.query.all()
+    couriers = Courier.query.all()
+    warehouses = Warehouse.query.all()
+    assignments = Assignment.query.all()
+    parcel_lockers = ParcelLocker.query.all()
 
+    return render_template(
+        "admin/db.html",
+        title="Database Overview",
+        users=users,
+        packages=packages,
+        couriers=couriers,
+        warehouses=warehouses,
+        assignments=assignments,
+        parcel_lockers=parcel_lockers,
+    )
