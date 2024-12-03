e7d779ff661513c754a39581a1db60fa272e9833
Martin Kondor <martinkondor@gmail.com>
2024-12-03 21:45:10 +0100

    add __str__ to some models

--- app/models/warehouse.py
+++ app/models/warehouse.py
@@ -1,12 +1,18 @@
 from app import db
 from datetime import datetime
 
+
 class Warehouse(db.Model):
-    __tablename__ = 'warehouse'
+    __tablename__ = "warehouse"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     address = db.Column(db.Text, nullable=False)
     capacity = db.Column(db.Integer, nullable=False)
     current_load = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+
+    def __str__(self):
+        return f"Warehouse(id={self.id}, name={self.name}, address={self.address}, capacity={self.capacity}, current_load={self.current_load})"
